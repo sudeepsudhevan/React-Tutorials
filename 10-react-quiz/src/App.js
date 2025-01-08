445fc6b6b0e1cf781e25b9a3c70741c4f2a7e160
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-08 12:30:50 +0530

    React quiz app loading data using useReducer hook

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,9 +1,53 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+
+  // "loading", "error", "ready", "active", "finished"
+  status: "loading",
+};
+
+function reducer(state, action) {
+  console.log(state, action);
+  
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      }
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      }
+    default:
+      throw new Error("Unknown action");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" })
+      );
+  }, []);
+
   return (
-    <div className="App">
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
