38afa0a4626e5a66f68852931383ec0c0c57bfbc
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-10 17:42:30 +0530

    Routes in react using react router dom

---
+++ 11-worldwise/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+
+
+export default function App() {
+  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
