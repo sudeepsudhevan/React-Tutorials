c3e117f6e60150204ba073d6922642d8d06fc86e
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-12 10:17:58 +0530

    Adding page and styless

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -4,6 +4,7 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- 11-worldwise/src/pages/PageNotFound.jsx
+++ 11-worldwise/src/pages/PageNotFound.jsx
@@ -1,11 +1,7 @@
-import PageNav from "../components/PageNav";
-
-
 export default function PageNotFound() {
   return (
     <div>
-      <PageNav />
-      <h1>Page Not Found</h1>
+      <h1>Page not found 😢</h1>
     </div>
-  )
+  );
 }
