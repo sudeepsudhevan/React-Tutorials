4211759f0289963d07bdedfc5fb45d74166ef5f6
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-12 11:20:56 +0530

    Nested route and index route

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -12,11 +12,16 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
         <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- 11-worldwise/src/components/AppNav.jsx
+++ 11-worldwise/src/components/AppNav.jsx
@@ -1,9 +1,17 @@
+import { NavLink } from "react-router-dom"
 import style from "./AppNav.module.css"
 
 export default function AppNav() {
   return (
     <nav className={style.nav}>
-        App Navigation
+        <ul>
+            <li>
+              <NavLink to="cities">Cities</NavLink>
+            </li>
+            <li>
+              <NavLink to="countries">Countries</NavLink>
+            </li>
+        </ul>
     </nav>
   )
 }
