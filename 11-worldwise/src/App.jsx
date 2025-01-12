4eda4b84397927dd7065a85439f9c7eb9cef312a
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-12 16:18:36 +0530

    Navigate component

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 import Homepage from "./pages/Homepage";
@@ -44,7 +44,7 @@ export default function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList cities={cities} isloading={isloading} />} />
+          <Route index element={<Navigate replace to="cities" />} />
           <Route path="cities" element={<CityList cities={cities} isloading={isloading} />} />
           <Route path="cities/:id" element={<City />} />
           <Route path="countries" element={<CountryList cities={cities} isloading={isloading} />} />
