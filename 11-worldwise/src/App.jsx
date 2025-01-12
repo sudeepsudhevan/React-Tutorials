e489920f8112031c1b2925c7f51e7134460a8290
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-12 16:13:10 +0530

    useNavigate in react

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -10,6 +10,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 
 const BASE_URL = "http://localhost:8000";
@@ -47,7 +48,7 @@ export default function App() {
           <Route path="cities" element={<CityList cities={cities} isloading={isloading} />} />
           <Route path="cities/:id" element={<City />} />
           <Route path="countries" element={<CountryList cities={cities} isloading={isloading} />} />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -1,14 +1,16 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 export default function Map() {
+  const navigate = useNavigate()
+
   const [searchParams, setSearchParams] = useSearchParams()
 
   const lat = searchParams.get("lat")
   const lng = searchParams.get("lng")
 
   return (
-    <div className={styles.mapContainer}>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
         <h1>Map</h1>
         <h1>Position: {lat}, {lng}</h1>
         <button onClick={() => setSearchParams({ lat: 76.727881642324164, lng: -19.140900099907554})}>
