9e7fbed5de89d2e6740e47883bb7948862d618ca
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-14 14:51:01 +0530

    worldwise Cityview completed

--- 11-worldwise/src/components/CityItem.jsx
+++ 11-worldwise/src/components/CityItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css"
+import { useCities } from "../contexts/CitiesContext";
 
 const formatDate = (date) =>
     new Intl.DateTimeFormat("en", {
@@ -9,12 +10,16 @@ const formatDate = (date) =>
     }).format(new Date(date));
 
 export default function CityItem({ city }) {
+
+    const { currentCity } = useCities();
     const { cityName, emoji, date, id, position } = city;
+
     // console.log(emoji);
     
     return (
         <li>
-            <Link to={`${id}?lat=${position.lat}&lng=${position.lng}`} className={styles.cityItem}>
+            <Link to={`${id}?lat=${position.lat}&lng=${position.lng}`} 
+                className={`${styles.cityItem} ${currentCity.id === id ? styles["cityItem--active"] : ""}`}>
                 <span className={styles.emoji}>{emoji}</span>
                 <h3 className={styles.name}>{cityName}</h3>
                 <time className={styles.date}>({formatDate(date)})</time>
--- 11-worldwise/src/contexts/CitiesContext.jsx
+++ 11-worldwise/src/contexts/CitiesContext.jsx
@@ -5,30 +5,45 @@ const BASE_URL = "http://localhost:8000";
 const CitiesContext = createContext();
 
 function CitiesProvider({ children }) {
-    const [cities, setCities] = useState([]);
-      const [isloading, setIsLoading] = useState(false);
-    
-      useEffect(function () {
-        async function fetchCities() {
-          try {
-            setIsLoading(true);
-            const res = await fetch(`${BASE_URL}/cities`);
-            const data = await res.json();
-            setCities(data);
-          } catch (err) {
-            alert("That was an error loading data");
-          } finally {
-            setIsLoading(false);
-          }
-        }
-        fetchCities();
-      }, []);
-    
-      return (
-        <CitiesContext.Provider value={{ cities, isloading }}>
-          {children}
-        </CitiesContext.Provider>
-      );
+  const [cities, setCities] = useState([]);
+  const [isloading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({})
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        alert("That was an error loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  async function getCity(id) {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch (err) {
+      alert("That was an error loading city data");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+
+  return (
+    <CitiesContext.Provider value={{ cities, isloading, currentCity, getCity }}>
+      {children}
+    </CitiesContext.Provider>
+  );
 }
 
 function useCities() {
