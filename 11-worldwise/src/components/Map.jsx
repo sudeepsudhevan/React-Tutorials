5a873fc281dfdbf806e743292daef7be3ce07cc4
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-15 16:25:31 +0530

    worldwise setting map with geolocation

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -3,27 +3,42 @@ import styles from "./Map.module.css";
 import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents } from "react-leaflet";
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
-import { map } from "leaflet";
+import { useGeolocation } from "../hooks/useGeolocation";
+import Button from "./Button";
+
 
 
 export default function Map() {
   const { cities } = useCities();
-
   const [mapPosition, setMapPosition] = useState([10.5, 76.29])
-
   const [searchParams] = useSearchParams()
+  const { isLoading: isLoadingPosition, position: geolocationPosition, getPosition } = useGeolocation(mapPosition);
 
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
-  useEffect(function() {
+  useEffect(function () {
     if (mapLat && mapLng) setMapPosition([mapLat, mapLng])
   }, [mapLat, mapLng])
 
+  useEffect(() => {
+    if (geolocationPosition && geolocationPosition.lat && geolocationPosition.lng) {
+      setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    } else {
+      console.error('Invalid geolocation position:', geolocationPosition);
+    }
+  }, [geolocationPosition]);
+
+
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type='position' onClick={getPosition}>
+          {isLoadingPosition ? "Detecting..." : "Use Your position"}
+        </Button>
+      )}
       <MapContainer className={styles.map}
-        center={mapPosition} 
+        center={mapPosition}
         zoom={6} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
