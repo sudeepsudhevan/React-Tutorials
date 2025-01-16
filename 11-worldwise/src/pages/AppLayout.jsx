509bc538278a9630af070e7926b8c1eca0e6d598
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-16 15:03:59 +0530

    worldwise implement the login

--- 11-worldwise/src/pages/AppLayout.jsx
+++ 11-worldwise/src/pages/AppLayout.jsx
@@ -1,6 +1,7 @@
 
 import Map from "../components/Map";
 import Sidebar from "../components/Sidebar";
+import User from "../components/User";
 import styles from "./AppLayout.module.css"
 
 export default function AppLayout() {
@@ -8,6 +9,7 @@ export default function AppLayout() {
     <div className={styles.app}>
       <Sidebar/>
       <Map/>
+      <User/>
     </div>
   )
 }
--- 11-worldwise/src/pages/Login.jsx
+++ 11-worldwise/src/pages/Login.jsx
@@ -1,16 +1,37 @@
+import { useNavigate } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/FakeAuthContext";
 import styles from "./Login.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Button from "../components/Button";
 
 export default function Login() {
-  // PRE-FILL FOR DEV PURPOSES
+
+  const { login, isAuthenticated } = useAuth();
+
   const [email, setEmail] = useState("sudeep@example.com");
   const [password, setPassword] = useState("qwerty");
+
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (email && password) {
+       login(email, password);
+    }
+  }
+
+  useEffect(function () {
+    if (isAuthenticated) {
+      return navigate("/app", { replace: true });
+    }
+  }, [isAuthenticated, navigate]);
 
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input
@@ -32,7 +53,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
