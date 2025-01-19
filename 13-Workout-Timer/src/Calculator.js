69138ffd71a7737eec641a07a073979100b09c4a
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-19 16:18:32 +0530

    workout timer stale closure problem

--- 13-Workout-Timer/src/Calculator.js
+++ 13-Workout-Timer/src/Calculator.js
@@ -23,6 +23,11 @@ function Calculator({ workouts, allowSound }) {
     playSound();
   }, [allowSound, duration]);
 
+  useEffect(() => {
+    document.title = `Your ${number}-exercise workout`;
+  }, [number]); // stale closure eliminates this dependency
+                // it is the previous value of number
+
   // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
