8577f87d9606155bd45162b376e5b2683ef73670
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-19 16:04:34 +0530

    workout timer useeffect usage

--- 13-Workout-Timer/src/Calculator.js
+++ 13-Workout-Timer/src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import clickSound from './ClickSound.m4a';
 
 function Calculator({ workouts, allowSound }) {
@@ -7,15 +7,33 @@ function Calculator({ workouts, allowSound }) {
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
 
-  const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+  const [duration, setDuration] = useState(0);
+
+  useEffect(() => {
+    const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
+    setDuration(duration);
+  }, [number, sets, speed, durationBreak]);
+
+  useEffect(function() {
+    const playSound = function() {
+      if (!allowSound) return;
+      const sound = new Audio(clickSound);
+      sound.play();
+    };
+    playSound();
+  }, [allowSound, duration]);
+
+  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
 
-  const playSound = function () {
-    if (!allowSound) return;
-    const sound = new Audio(clickSound);
-    sound.play();
-  };
+  function handleIncrement() {
+    setDuration((duration) => Math.floor(duration) + 1);
+  }
+
+  function handleDecrement() {
+    setDuration((duration) => (duration > 1 ? Math.ceil(duration) - 1 : 0));
+  }
 
   return (
     <>
@@ -66,13 +84,13 @@ function Calculator({ workouts, allowSound }) {
         </div>
       </form>
       <section>
-        <button onClick={() => {}}>–</button>
+        <button onClick={() => {handleDecrement()}}>–</button>
         <p>
           {mins < 10 && '0'}
           {mins}:{seconds < 10 && '0'}
           {seconds}
         </p>
-        <button onClick={() => {}}>+</button>
+        <button onClick={() => {handleIncrement()}}>+</button>
       </section>
     </>
   );
