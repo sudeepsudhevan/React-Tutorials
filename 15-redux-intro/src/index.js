b6e3f6e99cd43f08f3259779abcfd5ab9a56e578
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-20 12:47:18 +0530

    Redux file structure by slices

--- 15-redux-intro/src/index.js
+++ 15-redux-intro/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import './store'
+import store from './store';
+
+store.dispatch({ type: "customer/createCustomer", payload: { fullName: "John Doe", nationalID: "123456789" } });
+console.log(store.getState());
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
