46219ae9d9e704f138606e00b27cdcc47d00c422
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-20 13:03:30 +0530

    Redux connect with app

--- 15-redux-intro/src/index.js
+++ 15-redux-intro/src/index.js
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
 
-import store from './store';
+import store from "./store";
 
-store.dispatch({ type: "customer/createCustomer", payload: { fullName: "John Doe", nationalID: "123456789" } });
-console.log(store.getState());
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-
