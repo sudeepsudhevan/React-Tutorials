31d55971496380d8bf057e6187ca8f97058b9d20
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-20 11:38:59 +0530

    Redux action creator functions

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,60 +1,80 @@
 import { createStore } from "redux";
 
 const initialState = {
-    balance: 0,
-    loan: 0,
-    loanPurpose: "",
-}
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
 
 function reducer(state = initialState, action) {
-    switch (action.type) {
-        case "account/deposit":
-            return {
-            ...state,
-            balance: state.balance + action.payload,
-        }
-        case "account/withdraw":
-            return {
-            ...state,
-            balance: state.balance - action.payload,
-        }
-        case "account/requestLoan":
-            if (state.loan > 0) {
-                return state
-            }
-            return {
-            ...state,
-            loan: action.payload.amount,
-            loanPurpose: action.payload.purpose,
-            balance: state.balance + action.payload.amount,
-        }
-        case "account/payLoan":
-            return {
-            ...state,
-            loan: 0,
-            loanPurpose: "",
-            balance: state.balance - state.loan,
-        }
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "account/deposit":
+      return {
+        ...state,
+        balance: state.balance + action.payload,
+      };
+    case "account/withdraw":
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+      };
+    case "account/requestLoan":
+      if (state.loan > 0) {
+        return state;
+      }
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: "",
+        balance: state.balance - state.loan,
+      };
+    default:
+      return state;
+  }
 }
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 1000 });
+// store.dispatch({ type: "account/deposit", payload: 1000 });
+// console.log(store.getState());
+// store.dispatch({ type: "account/withdraw", payload: 500 });
+// console.log(store.getState());
+// store.dispatch({ type: "account/requestLoan", payload: { amount: 1000, purpose: "Education" } });
+// console.log(store.getState());
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
 
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount: amount, purpose: purpose },
+  };
+}
+
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(2000));
 console.log(store.getState());
-
-store.dispatch({ type: "account/withdraw", payload: 500 });
-
+store.dispatch(withdraw(600));
 console.log(store.getState());
-
-store.dispatch({ type: "account/requestLoan", payload: { amount: 1000, purpose: "Education" } });
-
+store.dispatch(requestLoan(1000, "Education"));
 console.log(store.getState());
-
-store.dispatch({ type: "account/payLoan" });
-
+store.dispatch(payLoan());
 console.log(store.getState());
-
