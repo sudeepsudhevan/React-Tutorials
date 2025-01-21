fbf89d2ce73bb3ea4f8897082d28b94b9b994396
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-21 09:49:58 +0530

    Redux dispatch actions

--- 15-redux-intro/src/features/accounts/AccountOperations.js
+++ 15-redux-intro/src/features/accounts/AccountOperations.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { deposit, withdraw, requestLoan, payLoan } from "./accountSlice";
 
 function AccountOperations() {
   const [depositAmount, setDepositAmount] = useState("");
@@ -7,13 +9,34 @@ function AccountOperations() {
   const [loanPurpose, setLoanPurpose] = useState("");
   const [currency, setCurrency] = useState("USD");
 
-  function handleDeposit() {}
+  const { loan: currentLoan, loanPurpose: currentLoanPurpose } = useSelector(
+    (store) => store.account
+  );
+  const dispatch = useDispatch();
 
-  function handleWithdrawal() {}
+  function handleDeposit() {
+    if (!depositAmount) return;
+    dispatch(deposit(depositAmount));
+    setDepositAmount("");
+  }
 
-  function handleRequestLoan() {}
+  function handleWithdrawal() {
+    if (!withdrawalAmount) return;
+    dispatch(withdraw(withdrawalAmount));
+    setWithdrawalAmount("");
+  }
 
-  function handlePayLoan() {}
+  function handleRequestLoan() {
+    if (!loanAmount || !loanPurpose) return;
+    dispatch(requestLoan(loanAmount, loanPurpose));
+    setLoanAmount("");
+    setLoanPurpose("");
+  }
+
+  function handlePayLoan() {
+    // if (account.loan === 0) return;
+    dispatch(payLoan());
+  }
 
   return (
     <div>
@@ -66,10 +89,14 @@ function AccountOperations() {
           <button onClick={handleRequestLoan}>Request loan</button>
         </div>
 
-        <div>
-          <span>Pay back $X</span>
-          <button onClick={handlePayLoan}>Pay loan</button>
-        </div>
+        {currentLoan > 0 && (
+          <div>
+            <span>
+              Pay back ${currentLoan} ({currentLoanPurpose}){" "}
+            </span>
+            <button onClick={handlePayLoan}>Pay loan</button>
+          </div>
+        )}
       </div>
     </div>
   );
--- 15-redux-intro/src/features/customers/Customer.js
+++ 15-redux-intro/src/features/customers/Customer.js
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 
 function Customer() {
   const customer = useSelector((store) => store.customer.fullName);
-  console.log(customer);
+  // console.log(customer);
 
   return <h2>👋 Welcome, {customer}</h2>;
 }
