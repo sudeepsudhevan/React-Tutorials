f352c0bb706fa8b5fc54049ed319a6e5afef0c0f
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-21 14:30:19 +0530

    Redux RTK creating account slice

--- 15-redux-intro/src/features/accounts/accountSlice.js
+++ 15-redux-intro/src/features/accounts/accountSlice.js
@@ -1,11 +1,58 @@
-const initialStateAccount = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
   isLoading: false,
 };
 
-export default function accountReducer(state = initialStateAccount, action) {
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    deposit(state, action) {
+      state.balance += action.payload;
+      state.isLoading = false;
+    },
+    withdraw(state, action) {
+      state.balance -= action.payload;
+    },
+    // if you want more than one argument in payload, you can use prepare
+    requestLoan: {
+      prepare(amount, purpose) {
+        return { payload: { amount, purpose } };
+      },
+
+      reducer(state, action) {
+        if (state.loan > 0) return;
+        console.log(state, action);
+
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance += action.payload.amount;
+      },
+    },
+    payLoan(state) {
+      state.balance -= state.loan;
+      state.loan = 0;
+      state.loanPurpose = "";
+    },
+    // convertingCurrency(state) {
+    //   state.isLoading = true;
+    // },
+  },
+});
+
+console.log(accountSlice.actions);
+
+export const { deposit, withdraw, requestLoan, payLoan, convertingCurrency } =
+  accountSlice.actions;
+
+export default accountSlice.reducer;
+
+/*
+export default function accountReducer(state = initialState, action) {
   switch (action.type) {
     case "account/deposit":
       return {
@@ -73,3 +120,5 @@ export function requestLoan(amount, purpose) {
 export function payLoan() {
   return { type: "account/payLoan" };
 }
+
+*/
