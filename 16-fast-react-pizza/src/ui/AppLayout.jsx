f6d49a05c2f17887c9aa9d770df7981c96d88abd
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-22 12:48:52 +0530

    Project 1 Loader indicator

--- 16-fast-react-pizza/src/ui/AppLayout.jsx
+++ 16-fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,10 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
+import Loader from "./Loader";
 
 export default function AppLayout() {
+  const navigation = useNavigation();
+  // console.log(navigation);
+  const isLoading = navigation.state === "loading";
+  
+
   return (
-    <div>
+    <div className="layout">
+
+        {isLoading && <Loader />}
         <Header />
         
         <main>
