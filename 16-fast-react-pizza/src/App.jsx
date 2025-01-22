5bd6c60a1b2a3b0ff1e90b5501cd6ac80ecaee66
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-22 11:30:24 +0530

    Project 1 Implement Routes using React-router-dom with createBrowserRouter

--- 16-fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/src/App.jsx
@@ -1,9 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./ui/Home"
+import Menu from "./features/menu/Menu"
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/menu',
+    element: <Menu />
+  },
+
+  {
+    path: '/cart',
+    element: <Cart />
+  },
+  {
+    path: '/order/new',
+    element: <CreateOrder />
+  },
+  {
+    path: '/order/:orderId',
+    element: <Order />
+  }
+]);
 
 export default function App() {
-  
-  return (
-    <div>
-      <h1>Fast React Pizza Co.</h1>
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
