b1ca204f02d20e39f7667ca509faf4ba481cc16d
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-22 12:38:49 +0530

    Project 1 fetching data with loader

--- 16-fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from "./ui/Home"
-import Menu from "./features/menu/Menu"
+import Menu,{ loader as menuLoader } from "./features/menu/Menu"
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
@@ -16,7 +16,8 @@ const router = createBrowserRouter([
       },
       {
         path: '/menu',
-        element: <Menu />
+        element: <Menu />,
+        loader: menuLoader,
       },
     
       { path: '/cart', element: <Cart />},
--- 16-fast-react-pizza/src/features/menu/Menu.jsx
+++ 16-fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,5 +1,26 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  console.log(menu);
+
+  return (
+    <ul>
+      {
+        menu.map((pizza) => (
+          <MenuItem key={pizza.id} pizza={pizza} />
+        ))
+      }
+    </ul>
+  );
+}
+
+// render as you fetch the data
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
