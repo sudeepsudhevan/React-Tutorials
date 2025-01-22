4a3342444df6800ef797f9fcac1c5e90eb2077cb
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-22 13:44:15 +0530

    Project 1 Fetching order

---
+++ 16-fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+export default function SearchOrder() {
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!query) return;
+        // navigate to the order page
+        navigate(`/order/${query}`);
+        setQuery("");
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                placeholder="Search orders #"
+            />
+        </form>
+
+    )
+}
