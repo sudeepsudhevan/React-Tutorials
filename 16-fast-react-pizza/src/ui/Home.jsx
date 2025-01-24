6d4901bcc65e4d61ccf84962af9cbc42201f5423
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-24 07:40:07 +0530

    Project 1 add color

--- 16-fast-react-pizza/src/ui/Home.jsx
+++ 16-fast-react-pizza/src/ui/Home.jsx
@@ -1,10 +1,12 @@
 function Home() {
   return (
     <div>
-      <h1 className="text-center text-xl text-yellow-500 font-semibold">
+      <h1 className="text-center text-xl text-stone-700 font-semibold">
         The best pizza.
         <br />
-        Straight out of the oven, straight to you.
+        <span className="text-yellow-500">
+          Straight out of the oven, straight to you.
+        </span>
       </h1>
     </div>
   );
