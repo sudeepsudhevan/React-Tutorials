a48c2d52eb3f142e49e05380825f8fe0a5999cf9
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-25 16:26:09 +0530

    Project 1 Absolute Postioning

--- 16-fast-react-pizza/src/ui/Loader.jsx
+++ 16-fast-react-pizza/src/ui/Loader.jsx
@@ -1,8 +1,11 @@
 
 export default function Loader() {
   return (
-    <div className="loader">
-        
+    <div 
+      className="absolute bg-slate-200/20 inset-0
+      backdrop-blur-sm flex items-center justify-center">
+      <div className="loader"></div>
     </div>
+
   )
 }
