65a601138fbf5a1f7b386e9aa9dbeddcdb6034dd
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-25 09:29:34 +0530

    Project 1 Style button state and transition

--- 16-fast-react-pizza/src/features/cart/Cart.jsx
+++ 16-fast-react-pizza/src/features/cart/Cart.jsx
@@ -29,7 +29,11 @@ function Cart() {
 
   return (
     <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+      <Link to="/menu"
+        className='text-sm text-blue-500 hover:text-blue-600'
+      >
+        &larr; Back to menu
+      </Link>
 
       <h2>Your cart, %NAME%</h2>
 
--- 16-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -74,15 +74,19 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+          // value={withPriority}
+          // onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>{isSubmitting ? 'Placing order...' : 'Order now'}</button>
+          <button disabled={isSubmitting}
+            className="bg-yellow-400 uppercase font-semibold text-stone-800 px-4 py-3 inline-block tracking-wide 
+            focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 rounded-full 
+            hover:bg-yellow-300 transition-colors duration-300 disabled:cursor-not-allowed"
+          >{isSubmitting ? 'Placing order...' : 'Order now'}</button>
         </div>
       </Form>
     </div>
