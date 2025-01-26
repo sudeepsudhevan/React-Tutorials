b0bdd359d8a79d3d837c524645e43c85684b24b1
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-26 06:57:38 +0530

    Project 1 custom font family

--- 16-fast-react-pizza/tailwind.config.js
+++ 16-fast-react-pizza/tailwind.config.js
@@ -1,12 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+    extend: {
+      colors: {
+        pizza: {
+          50: "#f9f9f9", // Add more shades
+          100: "#f4f4f4",
+          200: "#eaeaea",
+          300: "#e0e0e0",
+          400: "#c7c7c7",
+        },
+        height :{
+          screen: '100dvh',
+        }
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
