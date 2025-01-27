4a1f9b033148eba46b41712f818c0b0098da92f4
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-27 14:04:05 +0530

    Project 1 building the cart page

--- 16-fast-react-pizza/src/features/cart/Cart.jsx
+++ 16-fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,57 +1,38 @@
-import LinkButton from '../../ui/LinkButton';
-import Button from '../../ui/Button';
-import CartItem from './CartItem';
-import { use } from 'react';
-import { useSelector } from 'react-redux';
+import LinkButton from "../../ui/LinkButton";
+import Button from "../../ui/Button";
+import CartItem from "./CartItem";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart, getCart } from "./cartSlice";
+import { getUsername } from "../user/userSlice";
+import EmptyCart from "./EmptyCart";
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
 
 function Cart() {
-  const username = useSelector((state) => state.user.username);
+  const username = useSelector(getUsername);
+  const cart = useSelector(getCart);
+  const dispatch = useDispatch();
 
-  const cart = fakeCart;
+  if (!cart.length) return <EmptyCart />;
 
   return (
-    <div className='py-3 px-4'>
-      <LinkButton to="/menu">
-        &larr; Back to menu
-      </LinkButton>
+    <div className="py-3 px-4">
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2 className='mt-7 text-xl font-semibold'>Your cart, {username}</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
 
-      <ul className='divide-y divide-stone-200 mt-3 border-b'>
-        {
-          cart.map((item) => (
-            <CartItem item={item} key={item.pizzaId} />
-          ))
-        }
+      <ul className="divide-y divide-stone-200 mt-3 border-b">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.pizzaId} />
+        ))}
       </ul>
 
-      <div className='mt-6 space-x-2'>
-        <Button to="/order/new" type="primary">Order pizzas</Button>
-        <Button type="secondary">Clear cart</Button>
+      <div className="mt-6 space-x-2">
+        <Button to="/order/new" type="primary">
+          Order pizzas
+        </Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
