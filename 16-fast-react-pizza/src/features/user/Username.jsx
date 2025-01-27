b8d44528ea570a8175565212c2ca29f7cd8ffb58
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-27 08:49:50 +0530

    Project 1 model user state with react toolkit

--- 16-fast-react-pizza/src/features/user/Username.jsx
+++ 16-fast-react-pizza/src/features/user/Username.jsx
@@ -1,7 +1,16 @@
+import { useSelector } from "react-redux"
 
 
 export default function Username() {
+  const username = useSelector((state) => state.user.username)
+
+  if (!username) {
+    return null;
+  }
+
   return (
-    <div className="text-sm font-semibold hidden md:block">Sudeep</div>
+    <div className="text-sm font-semibold hidden md:block">
+      {username}
+    </div>
   )
 }
--- 16-fast-react-pizza/src/features/user/userSlice.js
+++ 16-fast-react-pizza/src/features/user/userSlice.js
@@ -1,3 +1,4 @@
+/*
 import { getAddress } from "../../services/apiGeocoding";
 
 function getPosition() {
@@ -21,3 +22,27 @@ async function fetchAddress() {
   // 3) Then we return an object with the data that we are interested in
   return { position, address };
 }
+*/
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  username: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers:{
+    updateName(state, action) {
+      state.username = action.payload
+    }
+  }
+})
+
+// console.log(userSlice);
+
+
+export const { updateName } = userSlice.actions;
+
+export default userSlice.reducer
