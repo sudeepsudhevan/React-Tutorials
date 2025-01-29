f2c2cfe4d1f00fd751b69e4d7d108d277375ca03
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-29 11:42:49 +0530

    Project 2 css function and props

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -2,12 +2,9 @@ import styled from "styled-components"
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -19,10 +16,13 @@ export default function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Check in and out</Heading>
         <Button onClick={() => console.log("Check in")}>Check in</Button>
         <Button onClick={() => console.log("Check out")}>Check out</Button>
 
+        <Heading as="h3">Form</Heading>
+        <Input type="number" placeholder="Number of guests" />
         <Input type="number" placeholder="Number of guests" />
       </StyledApp>
     </>
