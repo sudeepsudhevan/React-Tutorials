efa6a5b8b486634c635bd49bbe5ff90f66b43ba9
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-01-30 14:31:54 +0530

    Project 2 connecting superbase with react app

---
+++ 17-the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
+
+
+const supabaseUrl = 'https://jxfqtaudkigcewcvxmga.supabase.co'
+// eslint-disable-next-line no-undef
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export default supabase;
