849d50209b88a22e55afb250058ad32dcd393389
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-01 09:01:51 +0530

    Project 2 Create new cabin

--- 17-the-wild-oasis/src/services/apiCabins.js
+++ 17-the-wild-oasis/src/services/apiCabins.js
@@ -11,16 +11,27 @@ export async function getCabins() {
   return data;
 }
 
-export async function deleteCabin(id) {
-  const { data,error } = await supabase
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
     .from("cabins")
-    .delete()
-    .eq("id", id);
+    .insert([newCabin])
+    .select();
 
-    if (error) {
-      console.error(error);
-      throw new Error("Cabins could not be deleted");
-    }
-  
-  return data
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be created");
+  }
+
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be deleted");
+  }
+
+  return data;
 }
