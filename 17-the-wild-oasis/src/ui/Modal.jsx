aa4ea65777e2b5ecc3edab11a3f22bb61939899d
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-04 11:32:04 +0530

    Project 2 Detecting the Click outside

--- 17-the-wild-oasis/src/ui/Modal.jsx
+++ 17-the-wild-oasis/src/ui/Modal.jsx
@@ -1,7 +1,15 @@
-import { cloneElement, createContext, useContext, useState } from "react";
+import {
+  cloneElement,
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
+import { useOutsideClick } from "../hooks/useOutsideClick";
 
 const StyledModal = styled.div`
   position: fixed;
@@ -78,20 +86,23 @@ function Open({ opens: opensWindowName, children }) {
 }
 
 function Window({ children, name }) {
-
   const { openName, close } = useContext(ModalContext);
+
+  const ref = useOutsideClick(close);
 
   if (name !== openName) return null;
 
   return createPortal(
     <Overlay>
-      <StyledModal>
+      <StyledModal ref={ref}>
         <Button onClick={close}>
           <HiXMark />
         </Button>
-        <div>{cloneElement(children,{
-          onCloseModal:close
-        })}</div>
+        <div>
+          {cloneElement(children, {
+            onCloseModal: close,
+          })}
+        </div>
       </StyledModal>
     </Overlay>,
     document.body
