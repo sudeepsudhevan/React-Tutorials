0e17d7795cf5f3c842ef69b227355c936177eb27
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-04 11:01:11 +0530

    Project 2 Convert modal to Compound Component

--- 17-the-wild-oasis/src/features/cabins/AddCabin.jsx
+++ 17-the-wild-oasis/src/features/cabins/AddCabin.jsx
@@ -1,21 +1,41 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
+import CabinTable from "./CabinTable";
 import CreateCabinForm from "./CreateCabinForm";
 
 export default function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
   return (
-    <div>
-      <Button onClick={() => setIsOpenModal((show) => !show)}>
-        Add new cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)}/>
-        </Modal>
-      )}
-    </div>
-  );
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
+  )
 }
+
+// export default function AddCabin() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModal((show) => !show)}>
+//         Add new cabin
+//       </Button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateCabinForm onCloseModal={() => setIsOpenModal(false)}/>
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
--- 17-the-wild-oasis/src/ui/Modal-v1.jsx
+++ 17-the-wild-oasis/src/ui/Modal-v1.jsx
@@ -1,4 +1,3 @@
-import { cloneElement, createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
@@ -52,52 +51,16 @@ const Button = styled.button`
   }
 `;
 
-const ModalContext = createContext();
-
-function Modal({ children }) {
-  const [openName, setOpenName] = useState("");
-
-  const close = () => setOpenName("");
-  const open = setOpenName;
-
-  return (
-    <ModalContext.Provider value={{ close, open, openName }}>
-      {children}
-    </ModalContext.Provider>
-  );
-}
-
-function Open({ opens: opensWindowName, children }) {
-  const { open } = useContext(ModalContext);
-
-  return cloneElement(children, {
-    onClick: () => {
-      open(opensWindowName);
-    },
-  });
-}
-
-function Window({ children, name }) {
-
-  const { openName, close } = useContext(ModalContext);
-
-  if (name !== openName) return null;
-
+export default function Modal({ children, onClose }) {
   return createPortal(
     <Overlay>
       <StyledModal>
-        <Button onClick={close}>
+        <Button onClick={onClose}>
           <HiXMark />
         </Button>
-        <div>{cloneElement(children,{
-          onCloseModal:close
-        })}</div>
+        <div>{children}</div>
       </StyledModal>
     </Overlay>,
     document.body
   );
 }
-
-Modal.Open = Open;
-Modal.Window = Window;
-export default Modal;
