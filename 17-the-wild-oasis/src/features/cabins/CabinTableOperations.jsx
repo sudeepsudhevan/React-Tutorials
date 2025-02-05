56716957afdb00d6262334024cc25aecbd834842
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-05 08:22:04 +0530

    Project 2 Client Side Filtering: Filtering cabins

---
+++ 17-the-wild-oasis/src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,11 @@
+import TableOperations from "../../ui/TableOperations";
+import Filter from "../../ui/Filter";
+
+
+export default function CabinTableOperations() {
+  return (
+    <TableOperations>
+        <Filter filterField='discount' options={[{ value: 'all', label: 'All' }, { value: 'no-discount', label: 'No discount' }, { value: 'with-discount', label: 'With discount' }]} />
+    </TableOperations>
+  )
+}
--- 17-the-wild-oasis/src/pages/Cabins.jsx
+++ 17-the-wild-oasis/src/pages/Cabins.jsx
@@ -2,6 +2,7 @@ import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
 import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 
 function Cabins() {
 
@@ -9,7 +10,7 @@ function Cabins() {
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+        <CabinTableOperations />
       </Row>
 
       <Row>
