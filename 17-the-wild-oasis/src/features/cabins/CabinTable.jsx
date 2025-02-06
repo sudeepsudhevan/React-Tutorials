3cee2b4ac9adfb992fd14bc44cd7b4e3729e66f4
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-06 09:06:34 +0530

    Project 2 Client Side Sorting: Sorting cabins

--- 17-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 17-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -5,7 +5,6 @@ import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
 
-
 export default function CabinTable() {
   const { cabins, isLoading } = useCabins();
   const [searchParams] = useSearchParams();
@@ -14,6 +13,7 @@ export default function CabinTable() {
     return <Spinner />;
   }
 
+  // 1) Filter
   const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins;
@@ -30,6 +30,15 @@ export default function CabinTable() {
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
   }
 
+  // 2) Sort
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
+
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -43,7 +52,7 @@ export default function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
--- 17-the-wild-oasis/src/ui/Filter.jsx
+++ 17-the-wild-oasis/src/ui/Filter.jsx
@@ -53,6 +53,7 @@ export default function Filter({filterField, options}) {
             key={option.value}
             onClick={() => handleClick(option.value)}
             active={option.value === currentFilter}
+            disabled={option.value === currentFilter}
           >
             {option.label}
           </FilterButton>
