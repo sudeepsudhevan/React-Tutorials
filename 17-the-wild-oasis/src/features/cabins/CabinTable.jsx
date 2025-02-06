43739076c3f74a7d3447102c8a57c367cf23c26b
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-06 10:44:40 +0530

    Project 2 Building the Bookings Table

--- 17-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 17-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -4,6 +4,7 @@ import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 export default function CabinTable() {
   const { cabins, isLoading } = useCabins();
@@ -11,6 +12,10 @@ export default function CabinTable() {
 
   if (isLoading) {
     return <Spinner />;
+  }
+
+  if (!cabins.length) {
+    return <Empty resourceName="cabins" />;
   }
 
   // 1) Filter
