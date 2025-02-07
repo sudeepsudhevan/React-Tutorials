b1ae2fd9420dc964081c84e9f70f1fd1e2169e57
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-07 10:13:58 +0530

    Project 2 Prefetching with React Query

--- 17-the-wild-oasis/src/features/bookings/useBookings.js
+++ 17-the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,8 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
 
   // 1) Filter
@@ -19,18 +20,31 @@ export function useBookings() {
   const sortBy = { field, direction };
 
   // 3) Pagination
-  const page = !searchParams.get("page")
-    ? 1
-    : Number(searchParams.get("page"));
+  const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
+  // 4) Query
   const {
     isLoading,
-    data: {data: bookings, count} = {},
+    data: { data: bookings, count } = {},
     error,
   } = useQuery({
     queryKey: ["bookings", filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
+  // 5) Pre-fetching
+  const pageCount = Math.ceil(count / 10);
+
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
   return { isLoading, bookings, error, count };
 }
