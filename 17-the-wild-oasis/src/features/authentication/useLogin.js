bf0bb90cadc56a3fd8e8fd91c9a049370a5313de
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-09 12:14:03 +0530

    Project 2 User login with supabase Authentication

---
+++ 17-the-wild-oasis/src/features/authentication/useLogin.js
@@ -0,0 +1,24 @@
+import { useMutation } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export function useLogin() {
+  const navigate = useNavigate();
+
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => {
+      return loginApi({ email, password });
+    },
+    onSuccess: (user) => {
+      console.log("login success", user);
+      navigate("/dashboard");
+    },
+    onError: (err) => {
+      console.log("login error", err);
+      toast.error("Provided email or password is incorrect");
+    },
+  });
+
+  return { login, isLoading };
+}
