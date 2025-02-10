d75634bcd5faaa97ef731c8ce77a225a1f6f2039
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-10 10:48:47 +0530

    Project 2 Building the App Header

--- 17-the-wild-oasis/src/ui/Header.jsx
+++ 17-the-wild-oasis/src/ui/Header.jsx
@@ -1,16 +1,23 @@
 import styled from "styled-components";
-import Logout from "../features/authentication/Logout";
+import HeaderMenu from "./HeaderMenu";
+import UserAvatar from "../features/authentication/UserAvatar";
 
 const StyledHeader = styled.header`
-    background-color: var(--color-grey-0);
-    padding: 1.2rem 4.8rem;
-    border-bottom: 1px solid var(--color-grey-100);
+  background-color: var(--color-grey-0);
+  padding: 1.2rem 4.8rem;
+  border-bottom: 1px solid var(--color-grey-100);
+
+  display: flex;
+  gap: 2.4rem;
+  justify-content: flex-end;
+  align-items: center;
 `;
 
 export default function Header() {
   return (
     <StyledHeader>
-        <Logout />
+      <UserAvatar />
+      <HeaderMenu />
     </StyledHeader>
-  )
+  );
 }
