a185b419835488fecf11937b05de4d9bb6ece77b
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-10 19:21:29 +0530

    Project 2 Updating user data and password

--- 17-the-wild-oasis/src/features/authentication/SignupForm.jsx
+++ 17-the-wild-oasis/src/features/authentication/SignupForm.jsx
@@ -83,7 +83,7 @@ function SignupForm() {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button disabled={isLoading} variation="secondary" type="reset">
+        <Button onClick={() => reset()} disabled={isLoading} variation="secondary" type="reset">
           Cancel
         </Button>
         <Button disabled={isLoading}>Create new user</Button>
