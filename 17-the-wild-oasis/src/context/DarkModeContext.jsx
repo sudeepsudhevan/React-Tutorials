6b3d48e50da7b6d0c8613994f3440b9c16612cbf
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-11 13:03:31 +0530

    Project 2 Displaying a line chart with the richarts library

--- 17-the-wild-oasis/src/context/DarkModeContext.jsx
+++ 17-the-wild-oasis/src/context/DarkModeContext.jsx
@@ -1,6 +1,5 @@
 import { createContext, useContext, useEffect } from "react";
 import { useLocalStorageState } from "../hooks/useLocalStorageState";
-import { use } from "react";
 
 const DarkModeContext = createContext();
 
--- 17-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 17-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import Spinner from "../../ui/Spinner";
 import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -30,7 +31,7 @@ export default function DashboardLayout() {
       />
       <div>Today's Activity</div>
       <div>Chart stay durations</div>
-      <div>Chart sales</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
