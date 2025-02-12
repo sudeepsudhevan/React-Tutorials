b8a828fb748d1c01f8cb94cd89ac1297e679e08a
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-12 11:46:44 +0530

    work with event handler class based

--- 09-classy-weather/src/App.js
+++ 09-classy-weather/src/App.js
@@ -6,14 +6,31 @@ class Counter extends React.Component {
     this.state = {
       count: 0,
     };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this)
+  }
+
+  handleDecrement() {
+    this.setState((curState) => {
+      return { count: curState.count - 1 };
+    });
+  }
+
+  handleIncrement() {
+    this.setState((curState) => {
+      return { count: curState.count + 1 };
+    });
   }
 
   render() {
+    const date = new Date('june 21 2027');
+    date.setDate(date.getDate() + this.state.count);
+
     return (
       <div>
-        <button>-</button>
-        <span>{this.state.count}</span>
-        <button>+</button>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>{date.toDateString()} [{this.state.count}]</span>
+        <button onClick={this.handleIncrement}>+</button>
       </div>
     );
   }
