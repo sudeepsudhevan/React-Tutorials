72bbac26622cca9f0a1ce4cd3297c121afdf5279
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-12 15:39:33 +0530

    lifecyle methods

--- 09-classy-weather/src/App.js
+++ 09-classy-weather/src/App.js
@@ -37,14 +37,15 @@ function formatDay(dateStr) {
 class App extends React.Component {
 
   state = {
-    location: "thrissur",
+    location: "",
     isLoading: false,
     displayLocation: "",
     weather: {},
   };
 
-  // async fetchWeather() {
   fetchWeather = async () =>{
+    if(this.state.location.length < 2) return this.setState({weather:{}});
+
     try {
       this.setState({ isLoading: true });
       // 1) Getting location (geocoding)
@@ -70,7 +71,7 @@ class App extends React.Component {
       const weatherData = await weatherRes.json();
       this.setState({ weather: weatherData.daily });
     } catch (err) {
-      console.err(err);
+      console.error(err);
     } finally {
       this.setState({ isLoading: false });
     }
@@ -78,12 +79,28 @@ class App extends React.Component {
 
   setLocation = (e) => this.setState({ location: e.target.value })
 
+  // like useEffect []
+  componentDidMount(){
+    // this.fetchWeather();
+    this.setState({
+      location: localStorage.getItem('location') || ""
+    })
+  }
+
+  // like usEffect [location]
+  componentDidUpdate(prevProps, prevState){
+    if (this.state.location !== prevState.location){
+      this.fetchWeather();
+
+      localStorage.setItem('location', this.state.location)
+    }
+  }
+
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
         <Input location={this.state.location} onChangeLocation={this.setLocation} />
-        <button onClick={this.fetchWeather}>Get Weather</button>
 
         {this.state.isLoading && <p className="loader">Loading...</p>}
 
@@ -116,6 +133,12 @@ class Input extends React.Component{
 }
 
 class Weather extends React.Component {
+
+  componentWillUnmount(){
+    console.log("unmount");
+    
+  }
+
   render() {
     const {
       temperature_2m_max: max,
