077eb5ad3a4c21a67e281c1ed222dd34310d32ff
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-14 12:43:38 +0530

    Project 3 fetching data in a page

--- 21-the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/app/cabins/page.js
@@ -1,9 +1,22 @@
 
 
-export default function Page(){
+export default async function Page(){
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json();
+
+    console.log(data);
+    
+
     return (
         <div>
             <h1>Cabins pages</h1>
+
+            <ul>
+                {data.map((user) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
         </div>
     )
 }
