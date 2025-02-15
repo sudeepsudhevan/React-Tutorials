92c95053dca043bfa3956f77032724a46a368d17
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-15 11:03:33 +0530

    Project 3 Optimizing and loading fonts

--- 21-the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/app/layout.js
@@ -1,9 +1,12 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
+
+import { Josefin_Sans } from "next/font/google";
+const josefin = Josefin_Sans({ subsets: ["latin"], display: "swap" });
+
 import "@/app/_styles/globals.css";
 
 export const metadata = {
-  // title: "The Wild Oasis",
   title: {
     template: "%s / The Wild Oasis",
     default: "Welcome / The Wild Oasis",
@@ -14,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
         <Logo />
         <Navigation />
         <main>{children}</main>
