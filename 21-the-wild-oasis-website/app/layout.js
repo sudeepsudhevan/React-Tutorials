fdcce5d763bcde1d0a443410c3422ec1725a2f5e
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-15 09:40:52 +0530

    Project 3 Styling with tailwind

--- 21-the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/app/layout.js
@@ -1,15 +1,16 @@
 import Logo from "@/app/_components/Logo";
 import Navigation from "@/app/_components/Navigation";
+import "@/app/_styles/globals.css";
 
 export const metadata = {
   title: "The Wild Oasis",
   description: "Welcome to paradise",
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <Logo />
         <Navigation />
         <main>{children}</main>
