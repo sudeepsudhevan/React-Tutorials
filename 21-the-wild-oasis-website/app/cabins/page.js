01dfd6bcdc88953155f05ab6dcd0196bf5c72557
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-15 10:45:48 +0530

    Adding page metadata and favicon

--- 21-the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/app/cabins/page.js
@@ -1,5 +1,8 @@
 import Counter from "@/app/_components/Counter";
 
+export const metadata = {
+    title: "Cabins",
+}
 
 export default function Page(){
 
--- 21-the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/app/layout.js
@@ -3,8 +3,12 @@ import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 
 export const metadata = {
-  title: "The Wild Oasis",
-  description: "Welcome to paradise",
+  // title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description: "Luxurious cabin hotel,located in the heart of the wilderness.",
 };
 
 export default function RootLayout({ children }) {
