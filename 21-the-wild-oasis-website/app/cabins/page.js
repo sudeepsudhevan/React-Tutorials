9e645d8ae2b69278de7fd9ede29378077c1a38d1
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-16 12:52:03 +0530

    Project 3 Streaming UI with Suspense: Cabin List

--- 21-the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/app/cabins/page.js
@@ -1,13 +1,12 @@
-import CabinCard from "@/app/_components/CabinCard";
-import { getCabins } from "../_lib/data-service";
+import CabinList from "@/app/_components/CabinList";
+import { Suspense } from "react";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
-    title: "Cabins",
-}
+  title: "Cabins",
+};
 
-export default async function Page() {
-  const cabins = await getCabins();
-  
+export default function Page() {
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -17,19 +16,15 @@ export default async function Page() {
         Cozy yet luxurious cabins, located right in the heart of the Italian
         Dolomites. Imagine waking up to beautiful mountain views, spending your
         days exploring the dark forests around, or just relaxing in your private
-        hot tub under the stars. Enjoy nature&apos;s beauty in your own little home
-        away from home. The perfect spot for a peaceful, calm vacation. Welcome
-        to paradise.
+        hot tub under the stars. Enjoy nature&apos;s beauty in your own little
+        home away from home. The perfect spot for a peaceful, calm vacation.
+        Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+      {/* SUSPENSE need to be wrapped around the component and outside of the async function itself */}
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
-
