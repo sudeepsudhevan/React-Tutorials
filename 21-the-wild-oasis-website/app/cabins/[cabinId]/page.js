69b049132b9d4dc6a8e2c30cacb85cceccc80a05
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-16 14:10:17 +0530

    Project 3 Generating Dynamic Metadata

--- 21-the-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ 21-the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -2,6 +2,19 @@ import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
+// export const metadata = {
+//     title: "Cabin",
+// };
+
+export async function generateMetadata({ params }) {
+    const { cabinId } = await params;
+    const { name } = await getCabin(cabinId);
+    return {
+        title: `Cabin ${name}`,
+    };
+}
+
+
 export default async function Page({ params }) {
   const { cabinId } = await params;
 
@@ -14,7 +27,7 @@ export default async function Page({ params }) {
     <div className="max-w-6xl mx-auto mt-8">
       <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
         <div className="relative scale-[1.15] -translate-x-3">
-          <Image src={image} fill className="object-cover" alt={`Cabin ${name}`} />
+          <Image src={image} fill quality={100} className="object-cover" alt={`Cabin ${name}`} />
         </div>
 
         <div>
