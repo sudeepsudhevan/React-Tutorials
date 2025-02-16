6172bcac204295d8cd1e82a24bf73c0f2b3dbb22
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-16 11:33:23 +0530

    Project 3 fetching and displaying the cabin list

--- 21-the-wild-oasis-website/app/cabins/page.js
+++ 21-the-wild-oasis-website/app/cabins/page.js
@@ -1,13 +1,13 @@
 import CabinCard from "@/app/_components/CabinCard";
+import { getCabins } from "../_lib/data-service";
 
 export const metadata = {
     title: "Cabins",
 }
 
-export default function Page() {
-  // CHANGE
-  const cabins = [];
-
+export default async function Page() {
+  const cabins = await getCabins();
+  
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
--- 21-the-wild-oasis-website/next.config.mjs
+++ 21-the-wild-oasis-website/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: process.env.IMAGE_HOST,
+                port: '',
+                pathname: '/storage/v1/object/public/cabin-images/**',
+            }
+        ]
+    }
+};
 
 export default nextConfig;
