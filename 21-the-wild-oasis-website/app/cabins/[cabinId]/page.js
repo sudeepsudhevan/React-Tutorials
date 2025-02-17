e8030fe7f791e2f9c52410d9ee59c3491d706ca1
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-17 08:47:23 +0530

    Project 3 Client Components in Server Components

--- 21-the-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ 21-the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -1,3 +1,4 @@
+import TextExpander from "@/app/_components/TextExpander";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
@@ -7,18 +8,17 @@ import Image from "next/image";
 // };
 
 export async function generateMetadata({ params }) {
-    const { cabinId } = await params;
-    const { name } = await getCabin(cabinId);
-    return {
-        title: `Cabin ${name}`,
-    };
+  const { cabinId } = await params;
+  const { name } = await getCabin(cabinId);
+  return {
+    title: `Cabin ${name}`,
+  };
 }
 
 export async function generateStaticParams() {
-    const cabins = await getCabins();  
-    return cabins.map((cabin) => ({ cabinId: String(cabin.id) })); // returns an array of ids 
+  const cabins = await getCabins();
+  return cabins.map((cabin) => ({ cabinId: String(cabin.id) })); // returns an array of ids
 }
-
 
 export default async function Page({ params }) {
   const { cabinId } = await params;
@@ -32,7 +32,13 @@ export default async function Page({ params }) {
     <div className="max-w-6xl mx-auto mt-8">
       <div className="grid grid-cols-[3fr_4fr] gap-20 border border-primary-800 py-3 px-10 mb-24">
         <div className="relative scale-[1.15] -translate-x-3">
-          <Image src={image} fill quality={100} className="object-cover" alt={`Cabin ${name}`} />
+          <Image
+            src={image}
+            fill
+            quality={100}
+            className="object-cover"
+            alt={`Cabin ${name}`}
+          />
         </div>
 
         <div>
@@ -40,7 +46,9 @@ export default async function Page({ params }) {
             Cabin {name}
           </h3>
 
-          <p className="text-lg text-primary-300 mb-10">{description}</p>
+          <p className="text-lg text-primary-300 mb-10">
+            <TextExpander>{description}</TextExpander>
+          </p>
 
           <ul className="flex flex-col gap-4 mb-7">
             <li className="flex gap-3 items-center">
