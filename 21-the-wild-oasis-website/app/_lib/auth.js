6c4736818ac4a95c5eb9557072c0ff76d5fceb7d
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-18 13:47:56 +0530

    Project 3 Creating a New Guest on First Sign In

--- 21-the-wild-oasis-website/app/_lib/auth.js
+++ 21-the-wild-oasis-website/app/_lib/auth.js
@@ -1,6 +1,7 @@
 import { ca } from "date-fns/locale";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -10,8 +11,30 @@ const authConfig = {
     }),
   ],
   callbacks: {
-    authorized({ auth, request}) {
+    authorized({ auth, request }) {
       return !!auth?.user;
+    },
+    async signIn({ user }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest) {
+          await createGuest({
+            email: user.email,
+            fullName: user.name,
+          })
+        }
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+
+      session.user.guestId = guest.id;
+      return session;
     }
   },
   pages: {
--- 21-the-wild-oasis-website/app/account/page.js
+++ 21-the-wild-oasis-website/app/account/page.js
@@ -6,11 +6,13 @@ export const metadata = {
 
 export default async function page() {
   const session = await auth()
+  console.log(session);
+  
   const firstName = session.user.name.split(' ')[0]
 
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
         Welcome, {firstName}
-      </h2>
+    </h2>
   )
 }
