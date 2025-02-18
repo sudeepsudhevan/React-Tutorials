88757dc80498552e14e141e5f1c58a788ed341bb
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-18 12:58:52 +0530

    Project 3 Building a Custom Sign Out Button

--- 21-the-wild-oasis-website/app/_lib/actions.js
+++ 21-the-wild-oasis-website/app/_lib/actions.js
@@ -1,9 +1,15 @@
 "use server";
 
-import { signIn } from "./auth";
+import { signIn, signOut } from "./auth";
 
 export async function signInAction(){
     await signIn('google', {
         redirectTo: '/account',
+    });
+}
+
+export async function signOutAction(){
+    await signOut({
+        redirectTo: '/',
     });
 }
