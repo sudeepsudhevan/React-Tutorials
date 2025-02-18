6cf98d90de8fa70ada5c567305d5ebb66a25a713
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-18 09:57:12 +0530

    Project 3 Getting the user session

--- 21-the-wild-oasis-website/app/_components/Reservation.js
+++ 21-the-wild-oasis-website/app/_components/Reservation.js
@@ -1,12 +1,16 @@
+import { auth } from "@/app/_lib/auth";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import DateSelector from "./DateSelector";
 import ReservationForm from "./ReservationForm";
+import LoginMessage from "./LoginMessage";
 
 export default async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+  const session = await auth();
+
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
       <DateSelector
@@ -14,7 +18,7 @@ export default async function Reservation({ cabin }) {
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? <ReservationForm user={session.user} cabin={cabin} /> : <LoginMessage />}
     </div>
   );
 }
