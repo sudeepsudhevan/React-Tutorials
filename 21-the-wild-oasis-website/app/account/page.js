38507777a2fa2c9992d57a2164475e76a9da181b
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-18 12:24:42 +0530

    Project 3 Building a Custom Sign In Page

--- 21-the-wild-oasis-website/app/account/page.js
+++ 21-the-wild-oasis-website/app/account/page.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import { auth } from '@/app/_lib/auth'
 
 export const metadata = {
   title: "Guest Area",
 }
 
-export default function page() {
+export default async function page() {
+  const session = await auth()
+  const firstName = session.user.name.split(' ')[0]
+
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Welcome, Sudeep
+        Welcome, {firstName}
       </h2>
   )
 }
