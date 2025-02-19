0dbb428b5ec4d7a02c4c2a99a365c238fb421246
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-19 12:25:34 +0530

    Project 3 Updating a Reservation

--- 21-the-wild-oasis-website/app/_components/UpdateProfileForm.js
+++ 21-the-wild-oasis-website/app/_components/UpdateProfileForm.js
@@ -1,11 +1,9 @@
 "use client";
 
-import { useState } from "react";
 import { updateGuest } from "@/app/_lib/actions";
-import { useFormStatus } from "react-dom";
+import SubmitButton from "./SubmitButton";
 
 export default function UpdateProfileForm({ guest, children }) {
-  const [count, setCount] = useState(0);
 
   const { fullName, email, nationalID, nationality, countryFlag } = guest;
 
@@ -57,20 +55,12 @@ export default function UpdateProfileForm({ guest, children }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <Button />
+        <SubmitButton pendingLabel="Updating...">
+          Update profile
+        </SubmitButton>
       </div>
     </form>
   );
 }
 
-function Button() {
-  const { pending } = useFormStatus(); // client component
-  return (
-    <button
-      disabled={pending}
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-    >
-      {pending ? "Updating..." : "Update profile"}
-    </button>
-  );
-}
+
--- 21-the-wild-oasis-website/app/_lib/actions.js
+++ 21-the-wild-oasis-website/app/_lib/actions.js
@@ -4,6 +4,8 @@ import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
+
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -52,6 +54,45 @@ export async function deleteReservation(bookingId) {
   if (error) throw new Error("Reservation could not be deleted");
 
   revalidatePath("/account/reservations");
+}
+
+export async function updateReservation(formData) {
+
+  const bookingId = Number(formData.get("bookingId"));
+  // 1) Authenticate the user
+  const session = await auth();
+  if (!session) throw new Error("You must be signed in to update a reservation");
+
+  // 2) Authorization: Check if the user is allowed to update the reservation
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId)) {
+    throw new Error("You are not allowed to update this reservation");
+  }
+  
+  // 3) Build the update data
+  const updateData = {
+    numGuests : Number(formData.get("numGuests")),
+    observations : formData.get("observations").slice(0, 1000),
+  };
+
+  // 4) Update the reservation: mutate the data
+  const { error } = await supabase
+    .from("bookings")
+    .update(updateData)
+    .eq("id", bookingId);
+
+  // 5) Handle errors  
+  if (error) throw new Error("Reservation could not be updated");
+
+  // 6) Revalidate the page
+
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  revalidatePath("/account/reservations");
+
+  // 7) Redirecting the page
+  redirect("/account/reservations");
 }
 
 export async function signInAction() {
