9efe0a27a88c3a7fa52df8b1739d56a6e62b7982
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-19 14:48:20 +0530

    Project 3 Removing Reservations Immediately: The useOptimistic Hook

--- 21-the-wild-oasis-website/app/account/reservations/page.js
+++ 21-the-wild-oasis-website/app/account/reservations/page.js
@@ -1,4 +1,4 @@
-import ReservationCard from "@/app/_components/ReservationCard";
+import ReservationList from "@/app/_components/ReservationList";
 import { auth } from "@/app/_lib/auth";
 import { getBookings } from "@/app/_lib/data-service";
 import Link from "next/link";
@@ -9,9 +9,7 @@ export const metadata = {
 
 
 export default async function Page() {
-
   const session = await auth();
-  
   const bookings = await getBookings(session.user.guestId);
 
   return (
@@ -28,11 +26,7 @@ export default async function Page() {
           </Link>
         </p>
       ) : (
-        <ul className="space-y-6">
-          {bookings.map((booking) => (
-            <ReservationCard booking={booking} key={booking.id} />
-          ))}
-        </ul>
+        <ReservationList bookings={bookings} />
       )}
     </div>
   );
