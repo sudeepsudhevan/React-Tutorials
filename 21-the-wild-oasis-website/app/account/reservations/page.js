5cda44488f714c9ce1cd53c9275cd8136d61a1c8
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-19 09:16:58 +0530

    Project 3 Building the Guest's Reservations Page

--- 21-the-wild-oasis-website/app/account/reservations/page.js
+++ 21-the-wild-oasis-website/app/account/reservations/page.js
@@ -1,13 +1,18 @@
 import ReservationCard from "@/app/_components/ReservationCard";
+import { auth } from "@/app/_lib/auth";
+import { getBookings } from "@/app/_lib/data-service";
+import Link from "next/link";
 
 export const metadata = {
     title: "Reservations",
 }
 
 
-export default function Page() {
-  // CHANGE
-  const bookings = [];
+export default async function Page() {
+
+  const session = await auth();
+  
+  const bookings = await getBookings(session.user.guestId);
 
   return (
     <div>
@@ -18,9 +23,9 @@ export default function Page() {
       {bookings.length === 0 ? (
         <p className="text-lg">
           You have no reservations yet. Check out our{" "}
-          <a className="underline text-accent-500" href="/cabins">
+          <Link className="underline text-accent-500" href="/cabins">
             luxury cabins &rarr;
-          </a>
+          </Link>
         </p>
       ) : (
         <ul className="space-y-6">
