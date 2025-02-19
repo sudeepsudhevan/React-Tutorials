6ea48da79c0329497f89205e90e7644d066746ec
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-19 18:23:25 +0530

    Project 3 Back to the Cabin Page: Finishing the Date Selector

--- 21-the-wild-oasis-website/app/_components/DateSelector.js
+++ 21-the-wild-oasis-website/app/_components/DateSelector.js
@@ -1,6 +1,11 @@
 "use client";
 
-import { isWithinInterval } from "date-fns";
+import {
+  differenceInDays,
+  isPast,
+  isSameDay,
+  isWithinInterval,
+} from "date-fns";
 import { useState } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
@@ -17,16 +22,14 @@ function isAlreadyBooked(range, datesArr) {
 }
 
 function DateSelector({ settings, bookedDates, cabin }) {
-  
-  const {range, setRange, resetRange} = useReservation();
+  const { range, setRange, resetRange } = useReservation();
 
-  // CHANGE
-  const regularPrice = 23;
-  const discount = 23;
-  const numNights = 23;
-  const cabinPrice = 23;
+  const displayRange = isAlreadyBooked(range, bookedDates) ? {} : range;
 
-  // SETTINGS
+  const { regularPrice, discount } = cabin;
+  const numNights = differenceInDays(displayRange.to, displayRange.from);
+  const cabinPrice = (regularPrice - discount) * numNights;
+
   const { minBookingLength, maxBookingLength } = settings;
 
   return (
@@ -36,13 +39,17 @@ function DateSelector({ settings, bookedDates, cabin }) {
         mode="range"
         min={minBookingLength + 1}
         max={maxBookingLength}
-        selected={range}
+        selected={displayRange}
         onSelect={setRange}
         // fromMonth={new Date()}
         // fromDate={new Date()}
         // toYear={new Date().getFullYear() + 5}
         captionLayout="dropdown"
         numberOfMonths={2}
+        disabled={(curDate) =>
+          isPast(curDate) ||
+          bookedDates.some((date) => isSameDay(date, curDate))
+        }
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
