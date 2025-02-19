618803123c00e8f6ff54292cb3e708263cf1301b
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-19 08:42:14 +0530

    Project 3  Displaying a Loading Indicator: The useFormStatus Hook

--- 21-the-wild-oasis-website/app/_components/UpdateProfileForm.js
+++ 21-the-wild-oasis-website/app/_components/UpdateProfileForm.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { updateGuest } from "@/app/_lib/actions";
+import { useFormStatus } from "react-dom";
 
 export default function UpdateProfileForm({ guest, children }) {
   const [count, setCount] = useState(0);
@@ -9,7 +10,10 @@ export default function UpdateProfileForm({ guest, children }) {
   const { fullName, email, nationalID, nationality, countryFlag } = guest;
 
   return (
-    <form action={updateGuest} className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+    <form
+      action={updateGuest}
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
@@ -53,10 +57,20 @@ export default function UpdateProfileForm({ guest, children }) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <Button />
       </div>
     </form>
+  );
+}
+
+function Button() {
+  const { pending } = useFormStatus(); // client component
+  return (
+    <button
+      disabled={pending}
+      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+    >
+      {pending ? "Updating..." : "Update profile"}
+    </button>
   );
 }
