20b524acc0f1efb680e1dfa9b804c97cda703f2d
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-20 13:22:00 +0530

    Project 4 Api Route for message

---
+++ 22-nextjs-pages-router/pages/api/contact.js
@@ -0,0 +1,29 @@
+import { supabase } from "@/lib/supabase";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({success: false, error: "Please make a POST request" });
+  }
+  const contactData = {
+    fullName: "Sudeep",
+    email: "Sudeep@123",
+    subject: "Booking enquiry",
+    message: "This is a test message",
+  };
+
+  const { error } = await supabase.from("contact").insert([contactData]);
+
+  if (error) {
+    // Error message
+    return res.status(500).json({
+      success: false,
+      error: "Could not send message. Please try again",
+    });
+  }
+
+  // Success message
+  res.status(200).json({
+    success: true,
+    message: "Thanks for your message! we will get back to you soon :)",
+  });
+}
