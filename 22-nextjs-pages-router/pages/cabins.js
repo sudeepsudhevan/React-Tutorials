9f47ac69b2ae675bdec9b524c9078deede2ef3d2
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2025-02-20 09:15:59 +0530

    Project 4 Routes, Pages, and Navigation

---
+++ 22-nextjs-pages-router/pages/cabins.js
@@ -0,0 +1,7 @@
+
+
+export default function Cabins() {
+  return (
+    <div>cabins</div>
+  )
+}
--- 22-nextjs-pages-router/pages/index.js
+++ 22-nextjs-pages-router/pages/index.js
@@ -1,9 +1,14 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <div>
-      <h1>Next.js + Supabase</h1>
+      <h1 className="text-primary-700">Next.js + Supabase g</h1>
+      <div>
+        <Link href="/about">About</Link>
+        <Link href="/cabins">Cabins</Link>
+      </div>
     </div>
   );
 }
